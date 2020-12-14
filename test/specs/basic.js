57a245adefef88341787ed1dba90984e547e257f
gaudymejia <gaudy.mejia1008@gmail.com>
2020-12-14 12:43:36 -0600

    Send Basic Tests

--- test/specs/basic.js
+++ test/specs/basic.js
@@ -1,14 +1,16 @@
-describe('Parking Cost Calculator', () => {
-    it('should have the right title', async () => {
-        await browser.url('http://www.shino.de/parkcalc/')
-        await expect(browser).toHaveTitle('Parking Cost Calculator"');
-        console.log(browser.toHaveTitle());
-    })
+describe('For Valet Parking leaving date is before my starting date or time', () => {
+    it('should have a message when my leaving date is before my starting date or time', async () => {
+        await browser.url('http://www.shino.de/parkcalc/index.php?ParkingLot=Valet&StartingDate=12%2F1%2F2020&StartingTime=15%3A00&StartingTimeAMPM=AM&LeavingDate=11%2F1%2F2020&LeavingTime=07%3A00&LeavingTimeAMPM=AM&action=calculate&Submit=Calculate')
+        const messageResponse = $('<b>');  
+        await expect(messageResponse).toHaveTextContaining('ERROR! YOUR LEAVING DATE OR TIME IS BEFORE YOUR STARTING DATE OR TIME');
+    });
+
 })
 
-describe('My leaving date is before my starting date or time', () => {
-    it('should have the right title', async () => {
-        await browser.url('http://www.shino.de/parkcalc/index.php?ParkingLot=Valet&StartingDate=12%2F10%2F2020&StartingTime=12%3A00&StartingTimeAMPM=PM&LeavingDate=12%2F1%2F2020&LeavingTime=12%3A00&LeavingTimeAMPM=AM&action=calculate&Submit=Calculate')
-        await expect(browser).toHaveTitle('ERROR! YOUR LEAVING DATE OR TIME IS BEFORE YOUR STARTING DATE OR TIME');
+describe('When my departure time is less than my entry time on the same day', () => {
+    it('should have an error message', async () => {
+        await browser.url('http://www.shino.de/parkcalc/index.php?ParkingLot=Valet&StartingDate=12%2F1%2F2020&StartingTime=15%3A00&StartingTimeAMPM=AM&LeavingDate=12%2F1%2F2020&LeavingTime=07%3A00&LeavingTimeAMPM=AM&action=calculate&Submit=Calculate')
+        const messageResponse = $('<b>');     
+        await expect(messageResponse).toHaveText('ERROR!');
     })
 })
